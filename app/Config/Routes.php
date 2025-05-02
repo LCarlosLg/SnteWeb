41d697181e92727d6758e578b74c98462b8688e9
LCarlosLg <117101313+LCarlosLg@users.noreply.github.com>
2025-05-02 02:18:36 -0700

    Se modifico index.php para que hostinger se leean los datos correctamente

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,14 +5,14 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-$routes->get('/', 'Home::index');
+$routes->get('/', 'UsuariosController::login');
 
 // Rutas para el controlador UsuariosController
 $routes->get('login', 'UsuariosController::login');
 $routes->get('register', 'UsuariosController::register');
 $routes->post('usuarios/attemptLogin', 'UsuariosController::attemptLogin');
 $routes->post('usuarios/attemptRegister', 'UsuariosController::attemptRegister');
-//Vista de los productos
+// Vista de los productos
 $routes->get('productos', 'ProductosController::index');
 
 
